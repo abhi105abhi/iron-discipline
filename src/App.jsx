849a297d07ac264a8be31b2d5bf123685a571c5f
abhi105abhi <42896602+abhi105abhi@users.noreply.github.com>
2026-01-20 20:45:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Stats from './pages/Stats'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Stats from "./pages/Stats"
 
 export default function App() {
   return (
